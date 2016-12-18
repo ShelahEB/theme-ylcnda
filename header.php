b3b441026fdc16efe990381b326fa2bc9d0f3b04
Shelah Blando <shelah324@gmail.com>
2016-12-18 01:11:10 -0800

    connect header, sidebar, footer

--- header.php
+++ header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+
     <meta charset="<?php bloginfo('charset');?>"/>
+
     <title>
         <?php bloginfo('name');?> <?php wp_title();?>
     </title>
@@ -9,12 +11,24 @@
     <!--style sheet-->
     <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url');?>" />
 
+        <?php wp_head();?> <!--for plugins later-->
 </head>
 
-<body>
+<body <?php body_class():?>>
+
+    <div id="page">
+        <div id="header">
+            <h1>
+                <a href="<?php echo esc_url(home_url('/'));?>"
+                   title="<?php echo esc_attr(get_bloginfo('name', 'display'));?>"
+                   rel="home">
+                    <?php bloginfo('name');?>
+                </a>
+             </h1>
+
+            <h4><?php bloginfo('description');?></h4>
+
+        </div>
 
 
-</body>
 
-
-</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php get_header();?>
+<?php get_sidebar():?>
+<div id="content">
+
+
+
+
+
+</div>
+<?php get_footer();?>
