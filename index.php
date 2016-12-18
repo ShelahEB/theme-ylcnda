d243e10d61b8779c2d9e333bd13ceadc3cf3d464
Shelah Blando <shelah324@gmail.com>
2016-12-18 02:25:57 -0800

    index:simplified

--- index.php
+++ index.php
@@ -1,32 +1,24 @@
 <?php get_header();?>
-<?php get_sidebar();?>
-<div id="content"><!--start content div-->
-    <!--the loop-->
+<?php get_sidebar(); ?>
+<div id="content">
+<!--the loop-->
+        <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+        <div class="entry">
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <div id="content">
+                <?php the_content(); ?>
+                        <div class="post-metadata">
 
-    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+                                <span class="author">By <?php the_author_posts_link(); ?></span>
+                                <span class="date"><?php the_time('F jS, Y'); ?></span>
+                                <span class="categories">See more in: <?php the_category(' &middot; '); ?></span>
+                        </div>
+                        </div>
+        </div>
+                <?php endwhile; else: ?>
+        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+        <?php endif; ?>
+<!--end loop-->
 
-    <div class="entry"><!--start class entry-->
-        <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-
-        <div id="content"><!--start content-->
-            <div class="post-metadata"><!--start post-metadata-->
-                <span class="author">By <?php the_author_posts_link();?></span>
-                <span class="date"> <?php the_time('F jS, Y');?></span>
-                <span class="categories">See more in: <?php the_category('&middot; ');?></span>
-
-            </div><!--end post-metadata-->
-
-        </div><!--end content-->
-
-
-    </div><!--end class entry-->
-
-
-
-    <?php endwhile; else: ?>
-    <p><?php _('Sorry, no posts match your criteria.');?></p>
-    <?php endif;?>
-    <!--end loop-->
-</div><!--end content div-->
-<?php get_footer();?>
-
+</div>
+<?php get_footer(); ?>
