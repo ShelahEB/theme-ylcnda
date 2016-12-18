cf10dd6ac3e257299cd1610dd11ed57b35f2167f
Shelah Blando <shelah324@gmail.com>
2016-12-18 01:32:50 -0800

    footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<div id="footer"><!--start footer div-->
+    <cite>
+        &copy; <?php echo date('Y');?><a href="<?php bloginfor('url');?>">Ylucionada</a>.All rights reserved.
+
+    </cite>
+
+
+</div><!--end footer div-->
+
+<?php wp_footer(); ?>
+</div>
+</div>
+</html>
