88ecf6b5cede996588cf96d52867ba921f2dd2c2
Shelah Blando <shelah324@gmail.com>
2016-12-18 02:27:18 -0800

    simplified

--- header.php
+++ header.php
@@ -1,34 +1,18 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <title>
+                <?php bloginfo('name'); ?> <?php wp_title(); ?>
+        </title>
 
-    <meta charset="<?php bloginfo('charset');?>"/>
+        <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 
-    <title>
-        <?php bloginfo('name');?> <?php wp_title();?>
-    </title>
-
-    <!--style sheet-->
-    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url');?>" />
-
-        <?php wp_head();?> <!--for plugins later-->
+         <?php wp_head(); ?>
 </head>
-
-<body <?php body_class();?>>
-
-    <div id="page">
-        <div id="header">
-            <h1>
-                <a href="<?php echo esc_url(home_url('/'));?>"
-                   title="<?php echo esc_attr(get_bloginfo('name', 'display'));?>"
-                   rel="home">
-                    <?php bloginfo('name');?>
-                </a>
-             </h1>
-
-            <h4><?php bloginfo('description');?></h4>
-
-        </div>
-
-
-
+<body <?php body_class(); ?>>
+        <div id="page">
+                <div id="header">
+                        <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                        <h4><?php bloginfo('description'); ?></h4>
+                </div>
