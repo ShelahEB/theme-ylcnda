35db5a589d43ef194469d1903d2279d81c02a330
Shelah Blando <shelah324@gmail.com>
2016-12-18 02:28:43 -0800

    ...

--- page.php
+++ page.php
@@ -1,21 +1,17 @@
-<?php get_header(); ?>
+<?php get_header();?>
 <?php get_sidebar(); ?>
-<div id="content"><!--div content-->
-
-    <?php if (have_post()) : while(have_posts()) : the_post(); ?>
-
-        <div class="entry"><!--div entry-->
-            <h2><a href="<?php the_permalink();?>"<?php the_title(); ?></a></h2>
-
-            <div class="entry-body"><!--div entry body-->
+<div id="content">
+        <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+        <div class="entry">
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <div class="entry-body">
                 <?php the_content(); ?>
-            </div><!--end div entry body-->
 
-        </div><!--end div entry-->
-        <?php endwhile; else: ?>
-        <p><?php ('Sorry, no posts matched you criteria'):?></p>
+                        </div>
+        </div>
+                <?php endwhile; else: ?>
+        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
         <?php endif; ?>
 
-
-</div><!--edn div content-->
+</div>
 <?php get_footer(); ?>
