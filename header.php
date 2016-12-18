b7809d7b68a751dc4688d371a47a31de8df88545
Shelah Blando <shelah324@gmail.com>
2016-12-18 00:57:09 -0800

    meta + title tags

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset');?>"/>
+    <title>
+        <?php bloginfo('name');?> <?php wp_title();?>
+    </title>
+
+    <!--style sheet-->
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url');?>" />
+
+</head>
+
+<body>
+
+
+</body>
+
+
+</html>
